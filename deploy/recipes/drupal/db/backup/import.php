7035a7e980b429488acd458d4f63275d30e61cdc
Daniel Love <dlove@unleashed-technologies.com>
2021-08-31 13:40:11 -0600

    Updating drupal:db:backup:import recipe to use gunzip & drush sql:cli.

--- deploy/recipes/drupal/db/backup/import.php
+++ deploy/recipes/drupal/db/backup/import.php
@@ -1,9 +1,10 @@
 <?php
+
 namespace Deployer;
 
 task('drupal:db:backup:import', function () {
-  $latestBackup = run("find tmp -type f -printf '%T@ %p\n' | sort -n | tail -1 | cut -f2- -d\" \"");
-  vm("cd {{app_dir_name}} && zcat ../$latestBackup | drush sqlq");
+    $latestBackup = run("find tmp -type f -printf '%T@ %p\n' | sort -n | tail -1 | cut -f2- -d\" \"");
+    vm("cd {{app_dir_name}} && gunzip -c ../$latestBackup | drush sql:cli");
 })->desc('Import the latest database backup(s).')
-  ->local()
-  ->once();
+    ->local()
+    ->once();
