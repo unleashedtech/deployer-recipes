b6d8bf6208dbaa1e1cc6d272bd3f650301571810
Colin O'Dell <colinodell@gmail.com>
2021-08-31 18:38:42 -0400

    Test against UT's PHP coding standard

--- src/VirtualMachine.php
+++ src/VirtualMachine.php
@@ -1,23 +1,24 @@
 <?php
 
+declare(strict_types=1);
+
 namespace UnleashedTech\DeployerRecipes;
 
 /**
- * Class VirtualMachine.
  * @internal
  *
  * TODO: Define a `localhost` host that tells Deployer how to connect to the VM, so any task can be run within the VM.
  *       This may mean the web container needs to be configured to provide standard-fare SSH connectivity.
  */
-class VirtualMachine {
-    public static function run(string $command) {
-        if (is_file('.docksal/docksal.yml')) {
-            run("fin exec '$command'");
-        }
-        elseif (is_file('.ddev/config.yaml')) {
-            run("ddev exec '$command'");
-        }
-        else {
+class VirtualMachine
+{
+    public static function run(string $command): void
+    {
+        if (\is_file('.docksal/docksal.yml')) {
+            run(\sprint('fin exec %s', $command));
+        } elseif (\is_file('.ddev/config.yaml')) {
+            run(\sprint('ddev exec %s', $command));
+        } else {
             throw new \UnexpectedValueException('Unsupported VM.');
         }
     }
