fbaf045ea560ca25dc7d3e65ca4c5405ac86e111
Dan Shumaker <dshumaker@unleashed-technologies.com>
2021-09-17 18:47:07 -0400

    Updates regarding disussions and more testing from client for Import

--- cms/wp/db/backup/download.php
+++ cms/wp/db/backup/download.php
@@ -8,6 +8,7 @@
 namespace Deployer;
 
 task('wp:db:backup:download', function () {
-  $latestBackup = run("find {{backups}}/ -type f -printf '%T@ %p\n' | sort -n | tail -1 | cut -f2- -d\" \"");
-  download($latestBackup, "{{backups}}/$stage.sql.gz" );
+  $latestBackup = run("\ls -rt -1 {{backups}} | tail -1");
+  runLocally( 'mkdir -p {{local_backups}}' );
+  download("{{backups}}/$latestBackup", "{{local_backups}}/" );
 })->desc('Download the latest database backup.')->once();
--- cms/wp/db/backup/import.php
+++ cms/wp/db/backup/import.php
@@ -11,9 +11,11 @@ namespace Deployer;
 task(
 	'wp:db:backup:import',
 	function () {
-		$latestBackup = within( '{{backups}}', "find tmp -type f -printf '%T@ %p\n' | sort -n | tail -1 | cut -f2- -d\" \"" );
-		run( "cd {{app}} && zcat ../$latestBackup | {{wp}} db import " );
-		writeln( '<comment>> Imports db :<info>' . $latestBackup . '</info> </comment>' );
+		$latestBackup = run( "\ls -rt -1 {{local_backups}} | tail -1" );
+        // The issue with trying to re-use {{wp}} is that it is that the  machine environment is a remote server and so {{wp}} exists in a different
+        // directory.  The same justification necessitates {{local_backups}} definition
+		run( "cd web && {{local_wp}} db import ../{{local_backups}}/$latestBackup" );
+		writeln( '<comment>> Database file > <info>' . $latestBackup . '</info> has been imported locally! </comment>' );
 	}
 )->desc( 'Import the latest database backup.' )
   ->local()
