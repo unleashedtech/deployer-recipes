00434f156c9efdcc1445a94aaf304b6a2f13d965
Daniel Love <dlove@unleashed-technologies.com>
2021-09-20 11:33:36 -0600

    Adding composer:install recipe to allow custom composer install options. Adding related vars recipe. Revising deploy yaml to use the composer:install task. Revising global config to provide default composer install options. Updating change log.

---
+++ composer/vars.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Deployer;
+
+// Returns Composer binary path in found. Otherwise try to install latest
+// composer version to `.dep/composer.phar`. To use specific composer version
+// download desired phar and place it at `.dep/composer.phar`.
+set('composer', static function () {
+    if (test('[ -f {{deploy_path}}/.dep/composer.phar ]')) {
+        return '{{bin/php}} {{deploy_path}}/.dep/composer.phar';
+    }
+
+    if (commandExist('composer')) {
+        return '{{bin/php}} ' . locateBinaryPath('composer');
+    }
+
+    warning("Composer binary wasn't found. Installing latest composer to \"{{deploy_path}}/.dep/composer.phar\".");
+    run('cd {{deploy_path}} && curl -sS https://getcomposer.org/installer | {{bin/php}}');
+    run('mv {{deploy_path}}/composer.phar {{deploy_path}}/.dep/composer.phar');
+
+    return '{{bin/php}} {{deploy_path}}/.dep/composer.phar';
+});
