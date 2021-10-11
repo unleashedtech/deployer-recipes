ce1b433b3d39c2ae5c16beab64710d0b11640f08
Dan Shumaker <dshumaker@unleashed-technologies.com>
2021-10-11 15:46:22 -0400

    Fixing wp-cli specification.

--- cms/wp/db/backup/import.php
+++ cms/wp/db/backup/import.php
@@ -16,7 +16,7 @@ task(
         $latestBackup = runLocally('\ls -rt -1 {{local_database_backups}} | tail -1');
         // The issue with trying to re-use {{wp}} is that it is that the  machine environment is a remote server and so {{wp}} exists in a different
         // directory.  The same justification necessitates {{local_backups}} definition
-        runLocally('cd web && {{local_wp}} db import ../{{local_database_backups}}/' . $latestBackup);
+        runLocally('cd web && wp db import ../{{local_database_backups}}/' . $latestBackup);
         writeln('<comment>> Database file > <info>' . $latestBackup . '</info> has been imported locally! </comment>');
     }
 )->desc('Import the latest database backup.')
