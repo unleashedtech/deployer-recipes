c585bb6ef97ab85c1799a2de44481550ef9920bf
Daniel Love <dlove@unleashed-technologies.com>
2021-10-20 20:09:45 -0600

    Fixing database backup issue related to standardization of db:backup:download recipes.

--- cms/drupal/db/backup/create.php
+++ cms/drupal/db/backup/create.php
@@ -11,24 +11,28 @@ declare(strict_types=1);
 namespace Deployer;
 
 task('cms:drupal:db:backup:create', static function (): void {
-  // Ensure that the backup directory exists.
-    run('mkdir -p {{backups}}/latest');
+    // Ensure that the backup directory exists.
+    run('mkdir -p {{backups}}');
 
-  // Move files out of the "latest" backups folder.
-    run('[ "$(ls -A {{backups}}/latest)" ] && mv -v {{backups}}/latest/* {{backups}} || echo "No backups found in latest folder."');
+    // Make sure we have a current_path directory, otherwise pass.
+    $exists = test('[ -d {{current_path}} ]');
+    if (! $exists) {
+        // Pass. Don't need to do backup if it's the first time.
+        return;
+    }
 
-  // Create the backup file.
+    // Create the backup file.
     within(get('app_path'), static function (): void {
         $date = \date('Y-m-d--H-i-s');
-      // TODO: support exporting all sites found in site aliases list.
-      // $result = run("{{drush}} sa --format json");
-      // if (empty($result)) {
-      //   throw new \UnexpectedValueException('No site aliases found!');
-      // }
-      // $aliases = json_decode($result);
-      //foreach ($aliases as $alias => $info) {}
+        // TODO: support exporting all sites found in site aliases list.
+        // $result = run("{{drush}} sa --format json");
+        // if (empty($result)) {
+        //   throw new \UnexpectedValueException('No site aliases found!');
+        // }
+        // $aliases = json_decode($result);
+        //foreach ($aliases as $alias => $info) {}
 
         $filename = '{{namespace}}--' . $date . '.sql';
-        run('{{drush}} rq && {{drush}} sql:dump --gzip --result-file={{backups}}/latest/' . $filename . "|| echo 'Database not available'", ['timeout' => null]);
+        run('{{drush}} rq && {{drush}} sql:dump --gzip --result-file={{backups}}' . $filename . "|| echo 'Database not available'", ['timeout' => null]);
     });
 })->desc('Create a database backup files.')->once();
