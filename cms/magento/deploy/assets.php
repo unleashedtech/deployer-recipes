e42c8d43690766a069f68ebdae5a0def87aaebd4
Dan Shumaker <dshumaker@unleashed-technologies.com>
2021-11-05 17:43:20 -0400

    Initial Magento2 setup for Deployer7.

---
+++ cms/magento/deploy/assets.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Magento Deploy Assets
+ *
+ * @file
+ */
+
+declare(strict_types=1);
+
+namespace Deployer;
+
+task(
+    'magento:deploy:assets',
+    static function (): void {
+        $timestamp = \time();
+        run('{{magento}} setup:static-content:deploy --content-version=' . $timestamp . ' {{static_content_locales}}');
+    }
+)->desc('Deploy assets')->once();
