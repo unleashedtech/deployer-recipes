d1f2143f5d6bd62e1a751871c00a90231ce86879
Dan Shumaker <dshumaker@unleashed-technologies.com>
2022-01-13 16:53:01 -0500

    Adjusting sleep time and debugging.

--- cms/drupal/db/backup/import.php
+++ cms/drupal/db/backup/import.php
@@ -18,8 +18,8 @@ use UnleashedTech\DeployerRecipes\VirtualMachine;
 task('cms:drupal:db:backup:import', static function (): void {
     $latestBackup = runLocally('ls -tr -1 {{local_database_backups}} | tail -1');
     VirtualMachine::run('drush sql-drop -y');
-    runLocally('gzip -dfq {{local_database_backups}}/' . $latestBackup );
-    sleep(5); // Sleep 5seconds waiting for container sync to catch up.
+    runLocally('gzip -dfq {{local_database_backups}}/' . $latestBackup);
+    sleep(15); // Sleep 5seconds waiting for container sync to catch up.
     $unzipped = VirtualMachine::run('ls -tr -1 {{local_database_backups}} | tail -1');
     /** SET commands that are exported (mysqldump) from some mysql environments
         (often those coming from master slave environments ) do not import
@@ -32,8 +32,9 @@ task('cms:drupal:db:backup:import', static function (): void {
      *  It is harmless to strip these out for development purposes only.
      */
 
+    print $unzipped;
     VirtualMachine::run("perl -pi -e 's/SET @@.*//gd' {{local_database_backups}}/" . $unzipped);
-    VirtualMachine::run("drush -v sql:cli < {{local_database_backups}}/" .$unzipped);
+    VirtualMachine::run('drush -v sql:cli < {{local_database_backups}}/' . $unzipped);
     VirtualMachine::run('drush cr');
 })->desc('Import the latest database backup(s).')
     ->once();
