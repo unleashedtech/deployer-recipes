e7cd455fa3f57987f275bc99415714ea355d46dd
Dan Shumaker <dshumaker@unleashed-technologies.com>
2022-01-13 15:37:49 -0500

    Adding perl regex to strip out SET commands.

--- cms/drupal/db/backup/import.php
+++ cms/drupal/db/backup/import.php
@@ -18,7 +18,21 @@ use UnleashedTech\DeployerRecipes\VirtualMachine;
 task('cms:drupal:db:backup:import', static function (): void {
     $latestBackup = runLocally('ls -tr -1 {{local_database_backups}} | tail -1');
     VirtualMachine::run('drush sql-drop -y');
-    VirtualMachine::run('gunzip -c {{local_database_backups}}/' . $latestBackup . '| drush sql:cli');
+    VirtualMachine::run('gunzip -c {{local_database_backups}}/' . $latestBackup );
+    $unzipped = runLocally('ls -tr -1 {{local_database_backups}} | tail -1');
+    /** SET commands that are exported (mysqldump) from some mysql environments
+        (often those coming from master slave environments ) do not import
+        on single mysql containers and throw this error:
+     *
+     *  > ERROR 1227 (42000) at line 18: Access denied; you need (at least one
+     *  of) the SUPER, SYSTEM_VARIABLES_ADMIN or SESSION_VARIABLES_ADMIN
+     *  privilege(s) for this operation
+     *
+     *  It is harmless to strip these out for development purposes only.
+     */
+
+    VirtualMachine::run("perl -pi -e 's/SET @@.*//gd' " . $unzipped);
+    VirtualMachine::run("drush -v sql:cli < " .$unzipped);
     VirtualMachine::run('drush cr');
 })->desc('Import the latest database backup(s).')
     ->once();
