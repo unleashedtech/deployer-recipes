fde2e9259c90a2edac6d69688ccbbd642453c9a8
Dan Shumaker <dshumaker@unleashed-technologies.com>
2022-01-13 14:14:26 -0500

    removing -r option.

--- cms/drupal/db/backup/import.php
+++ cms/drupal/db/backup/import.php
@@ -18,7 +18,7 @@ use UnleashedTech\DeployerRecipes\VirtualMachine;
 task('cms:drupal:db:backup:import', static function (): void {
     $latestBackup = runLocally('ls -tr -1 {{local_database_backups}} | tail -1');
     VirtualMachine::run('drush sql-drop -y');
-    VirtualMachine::run('gunzip -c {{local_database_backups}}/' . $latestBackup . '| drush -r {{app_directory_name}} sql:cli');
-    VirtualMachine::run('drush -r {{app_directory_name}} cr');
+    VirtualMachine::run('gunzip -c {{local_database_backups}}/' . $latestBackup . '| drush sql:cli');
+    VirtualMachine::run('drush cr');
 })->desc('Import the latest database backup(s).')
     ->once();
